3fb344e2d28b7db8199f22bd01cbc6827f724d10
flan02 <chanivetdan@hotmail.com>
2023-08-24 19:24:47 -0300

    Trabajamos con layout y vimos como se pueden anidar y renderizarse por separado sin afectar al resto de la app, ademas de poder agregarles contextos para manejar solo cierta info dentro de cada layout. Continuaremos en el siguiente modulo con METADATA.

--- COURSE.md
+++ COURSE.md
@@ -6,3 +6,7 @@
 *aqui estaran todas las paginas de nuestra aplicacion*
 ## PUBLIC
 *elementos accedidos por el navegador, iconos o archivos para ver en el frontend*
+
+
+### LAYOUT
+*Con nextjs podemos crear varios layouts para componente que nosotros queramos englobar. Muy util en una aplicacion grande.*
---
+++ app/market/layout.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+//? El nav sera visible en todos los archivos que esten dentro de la carpeta market
+//* Podria crear un context que solo apliquen a los archivos dentro de la carpeta market
+
+const MarketRoot = ({ children }) => {
+  return (
+    <>
+      <nav>
+        <h3>Section Market</h3>
+        <ul>
+          <li>
+            <Link href="/market/categories">Categories</Link>
+          </li>
+          <Link href="/market/categories/technologies">Technologies</Link>
+          <li>
+            <Link href="/market/categories/sports">Sports</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+};
+
+export default MarketRoot;
