c873a21904d2c220ba75c516a457fce7c7f473af
flan02 <chanivetdan@hotmail.com>
2023-08-24 20:17:08 -0300

    Acabamos de ver notfound y como personalizar una pagina para cuando el usuario ingrese por url una pagina inexistente. Seguimos con REACT SERVER COMPONENTS.

---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+//TODO para que funcione este componente notfound cuando un usuario ingrese una ruta que no existe, se debe crear un archivo not-found.jsx en la carpeta app
+
+import Link from "next/link";
+
+export default function Notfound() {
+  return (
+    <>
+      <section>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <Link href="/">Volver</Link>
+      </section>
+    </>
+  );
+}
