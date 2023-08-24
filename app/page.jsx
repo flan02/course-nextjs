a99cacb8892cf12e853514a87cf35db732ac82be
flan02 <chanivetdan@hotmail.com>
2023-08-24 18:37:59 -0300

    Trabajamos con Link para cambiar de ruta sin refrescar la pagina. Continuamos con los usos del Layout.

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import Navbar from "../components/Navbar";
 //import styles from "./page.module.css";
+
+//! App ignora cualquier archivos que no se llame page o layout
+// ? Link reemplaza la etiqueta <a> y evita que se recargue la página
 
 export default function Home() {
   return (
     <>
       <h1>NEXTJS COURSE</h1>
-      <nav>
-        <h1>Navbar</h1>
-        <ul>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/market">Market</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
     </>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Navbar component</h1>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/market">Market</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
